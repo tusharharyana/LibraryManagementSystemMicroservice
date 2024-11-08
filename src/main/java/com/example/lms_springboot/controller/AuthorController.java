23f8b5a3ab435ea3c76a3784bcf86a0f5c957f8f
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2024-11-08 22:46:11 +0530

    Book and author schema created

---
+++ src/main/java/com/example/lms_springboot/controller/AuthorController.java
@@ -0,0 +1,4 @@
+package com.example.lms_springboot.controller;
+
+public class AuthorController {
+}
---
+++ src/test/java/com/example/lms_springboot/LmsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lms_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
